a3ed220bef9e991444d8165c2d7193594610a1dd
spyler <abhilash@localhost.localdomain>
2020-07-15 04:41:43 -0400

    version 1.0.3

--- Login/admin.py
+++ Login/admin.py
@@ -1,3 +1,10 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ['name', 'password']
+
+admin.site.register(User, UserAdmin)
